42306cd50227d0638158ce03c435928931f34864
Othavio Henrico <othavio.hs@aluno.ifsc.edu.br>
2023-11-21 20:53:43 -0300

    Created the dispose method on the home_page_view

--- lib/views/home/home_page_view.dart
+++ lib/views/home/home_page_view.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:virtual_library/views/home/home_bloc.dart';
 import 'package:virtual_library/views/home/home_states.dart';
 
-import 'package:flutter/material.dart';
-import 'package:virtual_library/views/home/home_bloc.dart';
-import 'package:virtual_library/views/home/home_states.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -26,43 +22,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Virtual Library')),
-      body: Column(
-        children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: () => _bloc.getBookList(),
-              child: const Text('Try Again'),
-            ),
-          ),
-          Expanded(
-            child: StreamBuilder<HomeState>(
-              stream: _bloc.getBookListStream,
-              builder:
-                  (BuildContext context, AsyncSnapshot<HomeState> snapshot) {
-                if (snapshot.data is SuccessHomeState) {
-                  final bookList = (snapshot.data as SuccessHomeState).bookList;
-                  return ListView.builder(
-                    itemCount: bookList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final book = bookList[index];
-                      return ListTile(
-                        key: Key(book.id.toString()),
-                        title: Text(book.title ?? ""),
-                        subtitle: Text(book.author ?? ""),
-                      );
-                    },
-                  );
-                } else if (snapshot.data is ErrorHomeState) {
-                  final message = (snapshot.data as ErrorHomeState).message;
-                  return Center(child: Text(message));
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
+      body: StreamBuilder<HomeState>(
+        stream: _bloc.getBookListStream,
+        builder: (BuildContext context, AsyncSnapshot<HomeState> snapshot) {
+          if (snapshot.data is SuccessHomeState) {
+            final bookList = (snapshot.data as SuccessHomeState).bookList;
+            return ListView.builder(
+              itemCount: bookList.length,
+              itemBuilder: (BuildContext context, int index) {
+                final book = bookList[index];
+                return ListTile(
+                  key: Key(book.id.toString()),
+                  title: Text(book.title ?? ""),
+                  subtitle: Text(book.author ?? ""),
+                );
               },
-            ),
-          )
-        ],
+            );
+          } else if (snapshot.data is ErrorHomeState) {
+            final message = (snapshot.data as ErrorHomeState).message;
+            return Center(child: Text(message));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
   }
 }
