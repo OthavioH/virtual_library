4eb11421b795d1ba475917f270d9f810f3c721eb
Othavio Henrico <othavio.hs@aluno.ifsc.edu.br>
2023-11-21 13:19:11 -0300

    Created Book model

---
+++ lib/models/book_model.dart
@@ -0,0 +1,28 @@
+
+class Book {
+  int? id;
+  String? title;
+  String? author;
+  String? coverUrl;
+  String? downloadUrl;
+
+  Book({this.id, this.title, this.author, this.coverUrl, this.downloadUrl});
+
+  Book.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    author = json['author'];
+    coverUrl = json['cover_url'];
+    downloadUrl = json['download_url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['author'] = author;
+    data['cover_url'] = coverUrl;
+    data['download_url'] = downloadUrl;
+    return data;
+  }
+}
