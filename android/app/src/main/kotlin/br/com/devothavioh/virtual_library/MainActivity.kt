31d59c2793f401773bfee0d96b37935b2288d0c5
Othavio Henrico <othavio.hs@aluno.ifsc.edu.br>
2023-11-21 13:18:41 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/devothavioh/virtual_library/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.devothavioh.virtual_library
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:virtual_library/views/home/home_page_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Virtual Library',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+      initialRoute: '/',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      )
+    );
+  }
+}
