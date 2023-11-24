ba81ac2871def94527b38a381d7e986ea161e7ef
Othavio Henrico <othavio.hs@aluno.ifsc.edu.br>
2023-11-23 21:53:52 -0300

    Added colors variable on virtual_library_colors.dart

--- lib/shared/themes/virtual_library_colors.dart
+++ lib/shared/themes/virtual_library_colors.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 class VirtualLibraryColors {
-  static const Color appPrimaryColor = Color(0xFF673ab7);
+  static const Color darkPurple = Color(0xFF673ab7);
+  static const Color lightPurple = Color(0xFFbe86f7);
+  static const Color purple = Color(0xFF8649f2);
+
+  static const Color appPrimaryColor = darkPurple;
 
   static const Color appBackgroundColor = Color(0xfff5f5f5);
 
-  static const Color tabBarIndicatorColors = Color.fromARGB(255, 190, 134, 247);
+  static const Color tabBarIndicatorColors = lightPurple;
+
+  static const Color favoriteIconColor = purple;
 }
